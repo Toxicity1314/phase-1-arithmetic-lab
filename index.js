54269163950ec52bb8c6aeecf636dfa931411507
Sean Welsh <s.welsh1314@gmail.com>
2022-11-18 16:37:37 -0500

    commit tree 85b83bc8f26db0cfd2b4032d1cb9f4b52254e19b

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31
+let num2 = 2
+let num3 = 16
+let num4 =6
 
-// Write your code here
+let multiply = num1*num2;
+let random = Math.floor(Math.random()*10) +1;
+const mod =  num3%num4;
+let max =  Math.max(10, 14, 20, 13, 1, 6)
